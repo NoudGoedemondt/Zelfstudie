9934a4605ff86144e0e6f7408c59e4878b042ed4
Noud Goedemondt <noud.goedemondt@pti.nl>
2024-02-27 16:16:24 +0100

    hopelijk werkt het nu

---
+++ bin/index.js
@@ -0,0 +1,3 @@
+"use strict";
+const noudIsCool = (x, y) => x + y;
+console.log(noudIsCool(1, 5));
---
+++ index.ts
@@ -0,0 +1,3 @@
+const noudIsCool = (x: number, y: number): number => x + y;
+
+console.log(noudIsCool(1, 5))
