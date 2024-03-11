39b537f5c1f1f4d54cc79fcf4427d89d5d30ae9a
Noud Goedemondt <noudgoedemondt@gmail.com>
2024-03-11 16:17:33 +0100

    kapotte versie teruggedraaid

--- bin/lec-1.js
+++ bin/lec-1.js
@@ -1,5 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const immutable_1 = require("immutable");
-const test = (0, immutable_1.List)([(0, immutable_1.List)([1, 2, 3, 4])]);
-console.log(test);
+let users = (0, immutable_1.Map)();
+const addUser = (newUser) => {
+    users = users.set(newUser.id, newUser);
+};
+addUser({ id: "6", name: "Noud", surname: "Goedemondt", age: 24 });
+addUser({ id: "7", name: "Deez", surname: "Nuts", age: 21 });
+console.log(users.get("6"));
--- src/lec-1.ts
+++ src/lec-1.ts
@@ -1,5 +1,12 @@
-import { List } from "immutable"
+import { List, set, Range, Map } from "immutable";
 
-const test: List<List<number>> = List([List([1,2,3,4])])
+type User = { id:string, name:string, surname:string, age:number }
+let users: Map<User["id"], User> = Map()
+const addUser = (newUser:User): void => {
+    users = users.set(newUser.id, newUser)
+}
 
-console.log(test)
+addUser({ id:"6", name:"Noud", surname:"Goedemondt", age:24 })
+addUser({ id:"7", name:"Deez", surname:"Nuts", age:21 })
+
+console.log(users.get("6"))
